28d52edd8f40da10b8b5a29921261302cfa39ac1
ashokmahanthi <ashokatusa2022@gmail.com>
2023-05-23 00:14:04 +0000

    seed data files changed as per requirement

--- seed-data/generate-votes.sh
+++ seed-data/generate-votes.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-# create 3000 votes (2000 for option a, 1000 for option b)
+# create 5000 votes (2000 for option a, 500 for option b, 1500 for option c, 1000 for option d)
 ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://vote/
-ab -n 1000 -c 50 -p postb -T "application/x-www-form-urlencoded" http://vote/
+ab -n 500 -c 50 -p postb -T "application/x-www-form-urlencoded" http://vote/
 ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://vote/
+ab -n 500 -c 50 -p postc -T "application/x-www-form-urlencoded" http://vote/
+ab -n 1000 -c 50 -p postd -T "application/x-www-form-urlencoded" http://vote/
+ab -n 1000 -c 50 -p postc -T "application/x-www-form-urlencoded" http://vote/
